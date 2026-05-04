6b95eeb739d2951c471d0b23edc13cfd80cbbfc2
Mark Watson <scrameta@gmail.com>
2026-05-04 21:33:27 +0200

    Added an FSMC memory tester

--- firmware/stm32testusbproject/AZURE_RTOS/App/app_azure_rtos.c
+++ firmware/stm32testusbproject/AZURE_RTOS/App/app_azure_rtos.c
@@ -28,6 +28,7 @@
 
 #include "logger.h"
 #include "thread_toggle.h"
+#include "thread_memtest.h"
 
 /* USER CODE END Includes */
 
@@ -131,6 +132,7 @@ VOID tx_application_define(VOID *first_unused_memory)
 
     /* USER CODE BEGIN MX_USBX_Host_Init_Success */
     thread_toggle_init();
+    thread_memtest_init();
 
     /* USER CODE END MX_USBX_Host_Init_Success */
   }
