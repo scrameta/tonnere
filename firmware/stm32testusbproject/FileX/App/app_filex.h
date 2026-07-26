65667c969c9bb380f1aab0407af66adc7925b585
Mark Watson <scrameta@gmail.com>
2026-07-26 10:42:42 +0200

    Added sd driver

--- firmware/stm32testusbproject/FileX/App/app_filex.h
+++ firmware/stm32testusbproject/FileX/App/app_filex.h
@@ -28,6 +28,7 @@ extern "C" {
 /* Includes ------------------------------------------------------------------*/
 #include "fx_api.h"
 
+#include "fx_stm32_sd_driver.h"
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
 
