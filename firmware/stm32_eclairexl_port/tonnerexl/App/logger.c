54cd1a69b2fd926dcc4f66ea2524611496d5461c
Mark Watson <scrameta@gmail.com>
2026-08-09 14:51:26 +0200

    Add timestamp to the logs

--- firmware/stm32_eclairexl_port/tonnerexl/App/logger.c
+++ firmware/stm32_eclairexl_port/tonnerexl/App/logger.c
@@ -6,15 +6,38 @@
 #include "platform.h"
 #include <stdarg.h>
 #include <stdio.h>
+#include "tx_api.h"
 
 void logger_init(void) { /* nothing to do; sink is set up by platform */ }
 
-void log_puts(const char *s) {
+static void log_puts_no_prefix(const char *s) {
     if (!s) return;
     while (*s) platform_log_putc(*s++);
 }
 
+static void prefix_time()
+{
+    ULONG ticks = tx_time_get();
+    char buf[32];
+    const char *s;
+    
+    uint32_t ms = (uint32_t)(((uint64_t)ticks * 1000U) / TX_TIMER_TICKS_PER_SECOND);
+
+    uint32_t seconds = ms / 1000U;
+    uint32_t millis  = ms % 1000U;
+
+    snprintf(&buf[0], 31, "%ld.%03ld: ", seconds, millis); 
+    s = &buf[0];
+    log_puts_no_prefix(s);
+}
+
+void log_puts(const char *s) {
+    prefix_time();
+    log_puts_no_prefix(s);
+}
+
 void log_printf(const char *fmt, ...) {
+    prefix_time();
     char buf[160];
     va_list ap;
     va_start(ap, fmt);
@@ -24,3 +47,4 @@ void log_printf(const char *fmt, ...) {
     if (n > (int)sizeof buf - 1) n = (int)sizeof buf - 1;  /* truncated */
     for (int i = 0; i < n; i++) platform_log_putc(buf[i]);
 }
+
