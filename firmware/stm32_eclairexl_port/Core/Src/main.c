a22a249bc512caf9f1f611b7fb19185940f0bdad
Mark Watson <scrameta@gmail.com>
2026-08-11 21:03:05 +0200

    Back to LSE

--- firmware/stm32_eclairexl_port/Core/Src/main.c
+++ firmware/stm32_eclairexl_port/Core/Src/main.c
@@ -117,7 +117,7 @@ int main(void)
   /* Initialize all configured peripherals */
   MX_GPIO_Init();
   MX_DMA_Init();
-  MX_RTC_Init();
+  //MX_RTC_Init();
   MX_USB_OTG_FS_HCD_Init();
   MX_FSMC_Init();
   MX_ADC1_Init();
@@ -163,9 +163,9 @@ void SystemClock_Config(void)
   /** Initializes the RCC Oscillators according to the specified parameters
   * in the RCC_OscInitTypeDef structure.
   */
-  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_LSI|RCC_OSCILLATORTYPE_HSE;
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE|RCC_OSCILLATORTYPE_LSE;
   RCC_OscInitStruct.HSEState = RCC_HSE_ON;
-  RCC_OscInitStruct.LSIState = RCC_LSI_ON;
+  RCC_OscInitStruct.LSEState = RCC_LSE_ON;
   RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
   RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
   RCC_OscInitStruct.PLL.PLLM = 8;
